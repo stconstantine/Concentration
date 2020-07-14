3044ae6c934b39f5342ca547e833ec079214d801
strkto <67833385+strkto@users.noreply.github.com>
2020-07-14 15:24:06 +0300

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Константин Стародубцев on 14.07.2020.
+//  Copyright © 2020 Константин Стародубцев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
