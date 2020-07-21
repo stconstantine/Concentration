dee731e70b1ba14d510323217548525647266113
strkto <67833385+strkto@users.noreply.github.com>
2020-07-21 15:26:09 +0300

    Lesson 3 finished. + access control + extension (for Int) + calculated properties + asserts + constraints

--- Concentration/Card.swift
+++ Concentration/Card.swift
@@ -11,11 +11,11 @@ import Foundation
 struct Card {
     var isFaceUp = false
     var isMatched = false
-    var identifier: Int
+    private(set) var identifier: Int
     
-    static var identifierFactory = 0
+    private static var identifierFactory = 0
     
-    static func getUniqueIdentifier() -> Int {
+    private static func getUniqueIdentifier() -> Int {
         identifierFactory += 1
         return identifierFactory
     }
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,18 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
-    var flipCount = 0 {didSet {flipCountLabel.text = "Flips: \(flipCount)"}}
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count+1)/2
+    }
+    
+    private(set) var flipCount = 0 {didSet {flipCountLabel.text = "Flips: \(flipCount)"}}
        
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.firstIndex(of: sender) {
-            print ("cardNumber = \(cardNumber)")
             game.chooseCard(at: cardNumber)
             updateViewFromModel()
         } else {
@@ -30,7 +33,7 @@ class ViewController: UIViewController {
     
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -44,16 +47,26 @@ class ViewController: UIViewController {
         }
     }
     
-    var emojiChoices = ["🎃","👻","🔥","🐶","😃","👔","👑","🍔","🥁","🎲","⏰","❤️","🦇","🦄"]
-    
-    var emoji = [Int:String]()
-
-    func emoji(for card: Card) -> String {
+    private var emojiChoices = ["🎃","👻","🔥","🐶","😃","👔","👑","🍔","🥁","🎲","⏰","❤️","🦇","🦄"]
+    private var emoji = [Int:String]()
+    private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
-                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            let randomIndex = emojiChoices.count.arc4random
                 emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
         }
-        print ("emoji.count = \(emoji.count)")
         return emoji[card.identifier] ?? "?"
+    }
+}
+
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+        
     }
 }
